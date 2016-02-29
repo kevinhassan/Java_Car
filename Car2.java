294ad498f393271cf7e5a0ba93f72ba0a14c1fe6
Kevin HASSAN <kevin.hassan@polytech.univ-montp2.fr>
2016-02-29 11:18:25 +0100

    Ajout du tourner en rond

--- Car2.java
+++ Car2.java
@@ -16,7 +16,7 @@ public class Car2
     //carImage.scale = 0.5;
     double consoEssence = 10.0;
     double essenceDansReservoir = 6.0;
-    
+    int x, y = 200;
     public Car2 () {}
     
     public Car2(String nomDuproprietaire) 
@@ -31,40 +31,35 @@ public class Car2
         carImage = new ColorImage( );
         consoEssence = newconsoEssence;
     }
-
-    public void moveForward(int dist)
-    { 
-        // Obtain the current rotation in degrees
-        int rotationInDegrees  = carImage.getRotation();
-        
-        // Tache 1 - Convertissez la rotation courante de degres en radians
-        // Initialisez la variable "rotationInRadians" avec une valeur correcte.
-        // ----------------------------------------------------
-        // Ecrivez votre code ici :
-        
-        double rotationInRadians= Math.toRadians(rotationInDegrees); // <- valeur temporaire a remplacer
-              // Tache 2 - Calculez la distance a parcourir en x et en y (en fonction de l'angle de rotation)
-        // Initialisez les variables distX et distY avec une valeur correcte.
-        // Voir enonce en pdf pour plus de details (cos, sin du module java Math)
-        // ----------------------------------------------------
-        // Ecrivez votre code ici :
-                     
-        double distX= dist * Math.cos(rotationInRadians),distY= dist * Math.sin(rotationInRadians); // <- valeurs temporaires a remplacer
-        
-        // ----------------------------------------------------
-        
-        
-        // Move the car in both x and y directions with the correct distances
-        // Notice that setX() and setY() take int as argument
-        carImage.setX(carImage.getX() + (int)distX);
-        carImage.setY(carImage.getY() + (int)distY);
-        
-        // Update the amount of gas in tank assumming that each unit of dist is 10m
-        double distKm = dist / 100.0;
-        double essenceUtilisee = distKm / 100.0 * consoEssence;
-        essenceDansReservoir = essenceDansReservoir - essenceUtilisee;
-        IO.outputln("Amount of gas used: " + essenceUtilisee + ", gas remained: " + essenceDansReservoir);
+    public Car2 (int posX, int posY)
+    {
+        x = posX;
+        y = posY;
     }
+
+
+    // Deplacer de dist pixels une voiture dans sa direction actuelle
+    public void moveForward(int dist)
+        { 
+            // Obtain the current rotation in degrees
+            int rotationInDegrees  = carImage.getRotation();
+            double rotationInRadians = rotationInDegrees*Math.PI/180; // <- valeur temporaire a remplacer
+           
+            // Computes the shift in the X and Y axes according to the rotation angle            
+            double distX = dist * Math.cos(rotationInRadians);
+            double distY = dist * Math.sin(rotationInRadians);
+           
+            // Move the car in both x and y directions with the correct distances
+            // Notice that setX() and setY() take int as argument
+            carImage.setX(carImage.getX() + (int)distX);
+            carImage.setY(carImage.getY() + (int)distY);
+            
+            // Update the amount of gas in tank assumming that each unit of dist is 10m
+            double distKm = dist / 100.0;
+            double essenceUtilisee = distKm / 100.0 * consoEssence;
+            essenceDansReservoir = essenceDansReservoir - essenceUtilisee;
+            IO.outputln("Amount of gas used: " + essenceUtilisee + ", gas remained: " + essenceDansReservoir);
+        }
     
     public void makeTurn(int angleAdditionnel) 
     { 
@@ -80,7 +75,30 @@ public class Car2
     public void Car2Demo()
     {
         Canvas canvas = new Canvas(); // declare une zone pour dessiner des objets
-        canvas.add(carImage,200,200); // ajoute l'image de la voiture aux coordonnees indiquees
+        canvas.add(carImage,x,y); // ajoute l'image de la voiture aux coordonnees indiquees
     }
+    
 
+    // Fait tourner une voiture (la vitesse est en pixels)
+    // le cercle effectue par la voiture est d'autant plus grand que la vitesse est grande
+    public void tournerEnRond(int vitesse) 
+    { 
+        int precision = 10;
+        for (int nb = 1; nb <= precision; nb++) 
+        {
+            moveForward(vitesse);
+            makeTurn(360/precision);
+            try{ Thread.sleep(300); } 
+            catch (Exception e) {} ;
+        }
+    }
+    
+    // Oriente une voiture dans la direction d'un objectif (dont les coordonnees sont donnees en param)
+    public void faireFace(int xObj, int yObj) 
+    {
+            // Methode de http://www.gamefromscratch.com/post/2012/11/18/GameDev-math-recipes-Rotating-to-face-a-point.aspx
+            double angle = Math.atan2(yObj - carImage.getY(), xObj - carImage.getX() );
+            angle = angle * (180/Math.PI);
+            carImage.setRotation((int)angle);
+   }    
 }
